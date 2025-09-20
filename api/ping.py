dabbb9f3ffd8c001221c079e8577573bbbca6016
anthoawad <anawad@student.ethz.ch>
2025-09-20 11:38:35 +0200

    test: add simple python function at /api/ping

---
+++ api/ping.py
@@ -0,0 +1,10 @@
+# api/ping.py
+from http.server import BaseHTTPRequestHandler
+
+class handler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header("content-type", "text/plain")
+        self.end_headers()
+        self.wfile.write(b"pong")
+        return
